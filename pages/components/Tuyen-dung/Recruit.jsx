9c5ee05737379955b3fed2d074ff495f8568f81b
quanghuydn94 <quanghuytrandn@gmail.com>
2022-03-03 21:29:21 +0700

    up code recruitment page

--- pages/components/Tuyen-dung/Recruit.jsx
+++ pages/components/Tuyen-dung/Recruit.jsx
@@ -10,36 +10,37 @@ export default function Recruit() {
           <h2>Giới thiệu</h2>
         </div>
         <ul>
-          <li style={{ color: "#649913" }}>
+          <li style={{ color: `var(--primary-color-)` }}>
             <Link href="/">Tin Tuyển dụng</Link>
           </li>
           <li style={{ color: "#666" }}>
-            <Link href="/Lien-he">Thông tin liên hệ</Link>
+            <Link href="/lien-he">Thông tin liên hệ</Link>
           </li>
         </ul>
       </div>
       <div className={styles.content}>
         <div className={styles.page_title}>Tuyển Dụng</div>
-        <div className={styles.project_item_bg}>
-          <div className={styles.img_desc}>
-            <Link href="/">
-              <img
-                src="https://hhlandgroup.com.vn/wp-content/uploads/2021/07/TD-QN-300x251.jpg"
-                alt=""
-              />
-            </Link>
-          </div>
-          <div className={styles.project_info}>
-            <Link href="/">
-              H&H LAND GROUP TUYỂN DỤNG THÁNG 7/2021 – CN QUẢNG NGÃI
-            </Link>
-            <div className={styles.split_bar_project}></div>
-            <div className={styles.project_content}>
-              {" "}
-              CHƯƠNG TRÌNH TUYỂN DỤNG LỚN NHẤT NĂM 2021 CỦA H&H […]
+        {Array.from({ length: 4 }).map((item, index) => (
+          <div key={index} className={styles.project_item_bg}>
+            <div className={styles.img_desc}>
+              <Link href="/">
+                <img
+                  src="https://hhlandgroup.com.vn/wp-content/uploads/2021/07/TD-QN-300x251.jpg"
+                  alt=""
+                />
+              </Link>
+            </div>
+            <div className={styles.project_info}>
+              <Link href="/">
+                H&H LAND GROUP TUYỂN DỤNG THÁNG 7/2021 – CN QUẢNG NGÃI
+              </Link>
+              <div className={styles.split_bar_project}></div>
+              <div className={styles.project_content}>
+                CHƯƠNG TRÌNH TUYỂN DỤNG LỚN NHẤT NĂM 2021 CỦA H&H […]
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
