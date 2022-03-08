6d692b03ea6d481828303e7137701d95e1056047
thien <thien.tipici@gmail.com>
2022-03-08 15:12:37 +0700

    clear

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,13 +5,13 @@ import Slider from "./Slider";
 import { useWindowScroll } from "react-use";
 function Home() {
   const { y: pageYOffset } = useWindowScroll();
-  useEffect(() => {
-    setTimeout(() => {
-      if (pageYOffset === 0) {
-        window.scrollTo({ top: 560, behavior: "smooth" });
-      }
-    }, [5000]);
-  }, [pageYOffset]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     if (pageYOffset === 0) {
+  //       window.scrollTo({ top: 560, behavior: "smooth" });
+  //     }
+  //   }, [5000]);
+  // }, [pageYOffset]);
   return (
     <div>
       <Slider />
