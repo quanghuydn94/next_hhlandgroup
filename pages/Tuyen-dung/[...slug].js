fad4eed4a1d3868fb77495afa212c44088d2a0dd
quanghuyTiPiCi <100989313+quanghuyTiPiCi@users.noreply.github.com>
2022-03-09 08:57:13 +0700

    Delete pages/Tuyen-dung directory

--- pages/Tuyen-dung/[...slug].js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import RecruitDetail from "../components/Recruitment/RecruitDetail";
-export default function RecruitmentDetail() {
-  return (
-    <div>
-      <RecruitDetail />
-    </div>
-  );
-}
